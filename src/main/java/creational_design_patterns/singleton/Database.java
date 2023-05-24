704d498b5c93d0236c4d88feb9103db1e5d46cd7
Md Sadman Hafiz <hafiz.sust333@gmail.com>
2023-05-24 14:53:54 +0600

    Update Database.java - Error Fixed,
    constructor Changed to private from public
    -In order to prevent from using new() operator.

--- src/main/java/creational_design_patterns/singleton/Database.java
+++ src/main/java/creational_design_patterns/singleton/Database.java
@@ -4,7 +4,7 @@ public class Database {
     public static Database databaseInstance;
     private String userName;
 
-    public Database(String userName){
+    private Database(String userName){
         this.userName = userName;
     }
 
